2c62fd95d0f3e6d134da7f240cce17727c463734
Nikita Parkhomenko <nikita.parkhom@gmail.com>
2020-06-30 13:20:26 +0300

    make build

---
+++ report/html_report/config.js
@@ -0,0 +1,31 @@
+report({
+  "testSuite": "BackstopJS",
+  "tests": [
+    {
+      "pair": {
+        "reference": "..\\bitmaps_reference\\Entire_document.png",
+        "test": "..\\bitmaps_test\\20200630-131850\\Entire_document.png",
+        "selector": "document",
+        "fileName": "Entire_document.png",
+        "label": "Entire document",
+        "requireSameDimensions": false,
+        "misMatchThreshold": 1,
+        "url": "http://localhost:8080/index.html",
+        "referenceUrl": "https://mate-academy.github.io/layout_solutions/hello-world/",
+        "expect": 0,
+        "viewportLabel": "custom-size",
+        "diff": {
+          "isSameDimensions": true,
+          "dimensionDifference": {
+            "width": 0,
+            "height": 0
+          },
+          "misMatchPercentage": "3.88",
+          "analysisTime": 12
+        },
+        "diffImage": "..\\bitmaps_test\\20200630-131850\\failed_diff_Entire_document.png"
+      },
+      "status": "fail"
+    }
+  ]
+});
